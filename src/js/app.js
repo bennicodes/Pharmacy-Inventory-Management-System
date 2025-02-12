6f6545084fa62fdbc739110aa1fe68b3a1c799db
Benedict Notoane <benni@MacBookPro.lan>
2025-02-12 15:02:46 +0100

    created add medicine modal structure and styling

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,1 @@
-import MedicineManager from "./medicineManager.js";
-import Medicine from "./medicine.js";
-
-const addMedicineForm = document.querySelector(".add-medicine__form");
-
-addMedicineForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  createMedicine();
-});
+// Selecting elements
