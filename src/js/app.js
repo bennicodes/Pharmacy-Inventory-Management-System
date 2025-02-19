f4c7ca0b76a8a0b6fb1a71cbf59aed0c4924163d
Benedict Notoane <benni@Benedicts-MacBook-Pro.local>
2025-02-19 17:37:57 +0100

    added open and close functionality to add medicine modal

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,14 @@
+import Ui from "./ui.js";
 // Selecting elements
+const addMedicineModal = document.querySelector(".add-medicine");
+const openAddMedicineModalButton = document.querySelector(
+  ".add-medicine__button"
+);
+const closeAddMedicineModalButton = document.querySelector(
+  ".add-medicine__close-button"
+);
+
+document.addEventListener("DOMContentLoaded", () => {
+  Ui.displayAddMedicine(openAddMedicineModalButton, addMedicineModal);
+  Ui.closeAddMedicineModal(closeAddMedicineModalButton, addMedicineModal);
+});
--- src/js/ui.js
+++ src/js/ui.js
@@ -1,5 +1,15 @@
-class Ui{
-    static displayAddMedicine(){
-        const 
-    }
+class Ui {
+  static displayAddMedicine(openAddMedicineModalButton, addMedicineModal) {
+    openAddMedicineModalButton.addEventListener("click", () => {
+      addMedicineModal.classList.add("display-add-medicine");
+    });
+  }
+
+  static closeAddMedicineModal(closeAddMedicineModalButton, addMedicineModal) {
+    closeAddMedicineModalButton.addEventListener("click", () => {
+      addMedicineModal.classList.remove("display-add-medicine");
+    });
+  }
 }
+
+export default Ui;
